2b84bca355873d4ed13bc330588540ad4fa0a2b7
kyle-berg <khb120@gmail.com>
2024-01-23 22:34:54 -0500

    made the grid and fiddled with drop downs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
+import './styles.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import  {useState} from 'react';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 
-function App() {
+
+function BasicExample() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Dropdown>
+      <Dropdown.Toggle variant="success" id="dropdown-basic">
+        Dropdown Button
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
   );
 }
 
-export default App;
+function Square() {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue('$player_name')
+  }
+
+  return (
+    <button 
+        className="square"
+        onClick={handleClick}
+      >
+        {value}
+      </button>
+    );
+}
+
+export default function Board() {
+  return (
+    <>
+    
+    <div className="board-row">
+        <BasicExample />
+        <Square />
+        <Square />
+        <Square />
+    </div>
+    <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+    </div>
+    <div className="board-row">
+        <Square />
+        <Square/>
+        <Square />
+    </div>
+    </>
+  );
+}
